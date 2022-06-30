a3d42d2aa08e07bb219db629ccc4fbe1c92000f3
fernandoleonid <fernandoleonid@gmail.com>
2022-06-30 13:18:49 -0300

    Projeto Login

---
+++ 04-login/js/login.js
@@ -0,0 +1,11 @@
+'use strict'
+
+const loginContainer = document.getElementById('login-container')
+
+const moveOverlay = () => loginContainer.classList.toggle('move')
+
+document.getElementById('open-register').addEventListener('click', moveOverlay)
+document.getElementById('open-login').addEventListener('click', moveOverlay)
+
+document.getElementById('open-register-mobile').addEventListener('click', moveOverlay)
+document.getElementById('open-login-mobile').addEventListener('click', moveOverlay)
